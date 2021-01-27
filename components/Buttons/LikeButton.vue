20ebeee8e6f62c063055f708059458b546a23a9e
Fernando Fernández <ferferga.fer@gmail.com>
2021-01-27 19:07:40 +0100

    feat: create new settings pages

--- components/Buttons/LikeButton.vue
+++ components/Buttons/LikeButton.vue
@@ -36,12 +36,12 @@ export default Vue.extend({
     }
   },
   mounted() {
-    this.$store.subscribe((mutation) => {
+    this.$store.subscribe((mutation, state) => {
       if (
         mutation?.type === 'SOCKET_ONMESSAGE' &&
-        mutation?.payload?.MessageType === 'UserDataChanged'
+        state.socket.message.MessageType === 'UserDataChanged'
       ) {
-        const payloadData = mutation?.payload?.Data?.UserDataList;
+        const payloadData = state.socket.message.Data.UserDataList;
         if (payloadData) {
           for (const payloadItem of payloadData) {
             if (payloadItem.ItemId === this.item.Id) {
